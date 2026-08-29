e04658fa38dfdd4fc39ffe2d8b1d40e0805f3a6c
EhudShi <165esh@gmail.com>
2026-08-29 11:50:31 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAYywK6ipnkO-F22JSTK2NL2wpQi4n0X4A",
+  authDomain: "hous-holds-by-me.firebaseapp.com",
+  projectId: "hous-holds-by-me",
+  storageBucket: "hous-holds-by-me.firebasestorage.app",
+  messagingSenderId: "1052012450420",
+  appId: "1:1052012450420:web:d714bc822fe287c63af721",
+};
